258923e22664cc98efb70e8f2fa577634b42fade
Elton Zheng <elton.zheng.sh@gmail.com>
2021-02-28 22:55:41 +0800

    Feat: Add Chinese locale and some components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Button, DatePicker, version, message, Alert } from 'antd';
 
-function App() {
+const App = () => {
+  const [date, setDate] = useState(null);
+  const onChange = (value) => {
+    message.info(
+      `您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`
+    );
+    setDate(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>antd version: {version}</h1>
+      <DatePicker onChange={onChange} />
+      <div style={{ marginTop: 16 }}>
+        <Alert
+          message='当前日期'
+          description={date ? date.format('YYYY年MM月DD日') : '未选择'}
+        />
+      </div>
+      <Button type='primary' style={{ marginLeft: 8 }}>
+        Primary Button
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ConfigProvider } from 'antd';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
